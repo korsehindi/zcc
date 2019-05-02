3c95d8ef37fdf99320bbdad726667bd5cbb87bfa
Tomoya Ishizaki <zaq1tomo@gmail.com>
2019-05-02 20:00:43 +0900

    [Step 1] Create the simplest compiler that compiles one number (#3)
    * テストケースを追加
    
    * 最もシンプルなコンパイラを実装
    
    * エラー用の関数を作成
    
    * コード生成用の関数を作成
    
    * macOS環境に対応
    
    * tmpファイルを無視

--- main.c
+++ main.c
@@ -1,6 +1,43 @@
 #include "zcc.h"
 
+void err(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+static void p(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vprintf(fmt, ap);
+  printf("\n");
+}
+
+static void tp(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  printf("\t");
+  vprintf(fmt, ap);
+  printf("\n");
+}
+
 int main(int argc, char **argv) {
-  printf("Hello, world\n");
+  if (argc != 2)
+    err("Usage: zcc <code>");
+
+  p(".intel_syntax noprefix");
+
+#ifdef __APPLE__
+  p(".global _main");
+  p("_main:");
+#else
+  p(".global main");
+  p("main:");
+#endif
+
+  tp("mov rax, %d", atoi(argv[1]));
+  tp("ret");
   return 0;
 }
--- test.sh
+++ test.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
+try() {
+  expected="$1"
+  input="$2"
+
+  ./zcc "$input" > tmp.s
+  gcc -o tmp tmp.s
+  ./tmp
+  actual="$?"
+
+  if [ "$actual" = "$expected" ]; then
+    echo "$input => $actual"
+  else
+    echo "$expected expected, but got $actual"
+    exit 1
+  fi
+}
+
+try 0 "0"
+try 123 "123"
+
 echo OK
