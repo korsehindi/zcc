70cc729422408e7c14c94784d481f8c5a53977f0
Tomoya Ishizaki <zaq1tomo@gmail.com>
2019-05-02 20:55:00 +0900

    [Step 4] Add "*" , "/" and "()" (#6)
    * テストケースを追加
    
    * ノードの型を定義
    
    * ASTを作成・解釈するように改善
    
    * 掛け算を実装
    
    * 割り算を実装
    
    * ()を実装

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include "zcc.h"
 
 Token tokens[100];
+int pos = 0;
+
+static Node *add();
 
 void err(char *fmt, ...) {
   va_list ap;
@@ -25,6 +28,21 @@ static void tp(char *fmt, ...) {
   printf("\n");
 }
 
+static Node *new_node(int op, Node *lhs, Node *rhs) {
+  Node *n = malloc(sizeof(Node));
+  n->ty = op;
+  n->lhs = lhs;
+  n->rhs = rhs;
+  return n;
+}
+
+static int consume(int ty) {
+  if (tokens[pos].ty != ty)
+    return 0;
+  pos++;
+  return 1;
+}
+
 void tokenize(char *s) {
   int i = 0;
 
@@ -33,7 +51,7 @@ void tokenize(char *s) {
       s++;
       continue;
     }
-    if (*s == '+' || *s == '-') {
+    if (strchr("+-*/()", *s)) {
       tokens[i].ty = *s;
       tokens[i].input = s;
       i++;
@@ -54,11 +72,83 @@ void tokenize(char *s) {
   tokens[i].input = s;
 }
 
+static Node *term() {
+  if (consume('(')) {
+    Node *n = add();
+    if (!consume(')'))
+      err(") expected, but got: %s", tokens[pos].input);
+    return n;
+  }
+  if (tokens[pos].ty == TK_NUM) {
+    Node *n = malloc(sizeof(Node));
+    n->ty = ND_NUM;
+    n->val = tokens[pos++].val;
+    return n;
+  }
+  err("invalid token %s", tokens[pos].input);
+  return NULL;
+}
+
+static Node *mul() {
+  Node *n = term();
+  for (;;) {
+    if (consume('*'))
+      n = new_node('*', n, term());
+    else if (consume('/'))
+      n = new_node('/', n, term());
+    else
+      return n;
+  }
+}
+
+static Node *add() {
+  Node *n = mul();
+  for (;;) {
+    if (consume('+'))
+      n = new_node('+', n, mul());
+    else if (consume('-'))
+      n = new_node('-', n, mul());
+    else
+      return n;
+  }
+}
+
+void gen(Node *n) {
+  if (n->ty == ND_NUM) {
+    tp("push %d", n->val);
+    return;
+  }
+
+  gen(n->lhs);
+  gen(n->rhs);
+  tp("pop rdi");
+  tp("pop rax");
+
+  switch (n->ty) {
+  case '+':
+    tp("add rax, rdi");
+    break;
+  case '-':
+    tp("sub rax, rdi");
+    break;
+  case '*':
+    tp("mul rdi");
+    break;
+  case '/':
+    tp("mov rdx, 0");
+    tp("div rdi");
+    break;
+  }
+
+  tp("push rax");
+}
+
 int main(int argc, char **argv) {
   if (argc != 2)
     err("Usage: zcc <code>");
 
   tokenize(argv[1]);
+  Node *node = add();
 
   p(".intel_syntax noprefix");
 
@@ -70,32 +160,8 @@ int main(int argc, char **argv) {
   p("main:");
 #endif
 
-  int i = 1;
-
-  if (tokens[0].ty != TK_NUM)
-    err("invalid token: %s", tokens[0].input);
-  tp("mov rax, %d", tokens[0].val);
-
-  while (tokens[i].ty != TK_EOF) {
-    if (tokens[i].ty == '+') {
-      i++;
-      if (tokens[i].ty != TK_NUM)
-        err("invalid token: %s", tokens[i].input);
-      tp("add rax, %d", tokens[i].val);
-      i++;
-      continue;
-    }
-    if (tokens[i].ty == '-') {
-      i++;
-      if (tokens[i].ty != TK_NUM)
-        err("invalid token: %s", tokens[i].input);
-      tp("sub rax, %d", tokens[i].val);
-      i++;
-      continue;
-    }
-    err("invalid token: %s", tokens[i].input);
-  }
-
+  gen(node);
+  tp("pop rax");
   tp("ret");
   return 0;
 }
--- zcc.h
+++ zcc.h
@@ -2,6 +2,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 enum {
   TK_NUM = 256,
@@ -13,3 +14,14 @@ typedef struct {
   int val;
   char *input;
 } Token;
+
+enum {
+  ND_NUM = 256,
+};
+
+typedef struct Node {
+  int ty;
+  int val;
+  struct Node *lhs;
+  struct Node *rhs;
+} Node;
